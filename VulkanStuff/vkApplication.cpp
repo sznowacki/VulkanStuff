6049f4ce492bbaa3cdeed8acecf0a9179fb8cdec
ssnow <ss.nowacki@gmail.com>
2020-12-06 18:25:03 +0100

    [VK] Add CreateInstance() and enumeration for supported instance extensions

--- VulkanStuff/vkApplication.cpp
+++ VulkanStuff/vkApplication.cpp
@@ -1,6 +1,7 @@
 ﻿#include "vkApplication.h"
-#define GLFW_INCLUDE_VULKAN
-#include <GLFW/glfw3.h> //GLFW automatically include Vulkan header
+
+#include <stdexcept>
+#include <vector>
 
 void vkApplication::Run()
 {
@@ -12,7 +13,59 @@ void vkApplication::Run()
 
 void vkApplication::InitVulkan()
 {
-	
+	CreateInstance();
+}
+
+void vkApplication::EnumerateSupportedInstanceExtensions()
+{
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    std::vector<VkExtensionProperties> extensions(extensionCount);
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+    printf("Available Extensions: \n");
+    for(const auto& extension : extensions)
+    {
+        printf("\t%s\n", extension.extensionName);
+    }
+}
+
+void vkApplication::CreateInstance()
+{
+	VkApplicationInfo vkApplicationInfo
+    {
+		VK_STRUCTURE_TYPE_APPLICATION_INFO,
+		nullptr,
+		"vkTriangle",
+		VK_MAKE_VERSION(1,0,0),
+		"No Engine",
+		VK_MAKE_VERSION(1,0,0),
+		VK_API_VERSION_1_2
+	};
+
+	uint32_t glfwExtensionCount = 0;
+	const char** glfwExtensions = nullptr;
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+	VkInstanceCreateInfo vkInstanceCreateInfo
+    {
+		VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+		nullptr,
+		NULL,
+		&vkApplicationInfo,
+		0,
+		nullptr,
+		glfwExtensionCount,
+		glfwExtensions
+	};
+
+	EnumerateSupportedInstanceExtensions();
+
+	if(vkCreateInstance(&vkInstanceCreateInfo, nullptr, &vkInstance) != VK_SUCCESS)
+	{
+		throw std::runtime_error("Failed to create instance!");
+	}
 }
 
 void vkApplication::InitWindow()
@@ -36,6 +89,8 @@ void vkApplication::MainLoop()
 
 void vkApplication::Cleanup()
 {
+	vkDestroyInstance(vkInstance, nullptr);
+
 	glfwDestroyWindow(window);
 
 	glfwTerminate();
--- VulkanStuff/vkApplication.h
+++ VulkanStuff/vkApplication.h
@@ -1,20 +1,30 @@
 ﻿#pragma once
 #include <cstdint>
 
-class GLFWwindow;
+//GLFW automatically include Vulkan header
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h> 
 
 class vkApplication
 {
 public:
-	void Run();
-
+	//Members
     GLFWwindow* window = nullptr;
 
     const uint32_t WINDOW_WIDTH = 800;
 	const uint32_t WINDOW_HEIGHT = 600;
 
+	//Methods
+	void Run();
+
 private:
+	//Members
+	VkInstance vkInstance = nullptr;
+
+	//Methods
 	void InitVulkan();
+    void EnumerateSupportedInstanceExtensions();
+    void CreateInstance();
 	void InitWindow();
 	void MainLoop();
 	void Cleanup();
